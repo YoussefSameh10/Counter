8365b7e40f1128e8e0a95d20901b71cf84303efe
youssef.ghattas <youssef.ghattas@integrant.com>
2022-09-18 13:20:58 +0200

    initial commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Youssef Ghattas on 18/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Youssef Ghattas on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
